4b13e426cfb13d2bcf42c969e858b0d34bcdf1fa
carlos.daeli <carlosdaeli@gmail.com>
2024-04-27 18:34:15 +0700

    Membangun Web Services Menggunakan Node.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// const message = (name) => {
+//     console.log(`Welcome ${name}`);
+// }
+//
+// message("JavaScript")
+
+const moment = require('moment');
+
+const date = moment().format("MMM Do YY");
+console.log(date);
